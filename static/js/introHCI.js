06911ea73c54436b752e5fa03cca51ba5173db6c
Michael Lam <mel157@ucsd.edu>
2019-01-24 11:28:42 -0800

    All changed files

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,8 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
+
+	function projectClick(e) {
+		e.preventDefault();
+		var containingProject = $(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+		if (description.length == 0) {
+			$(containingProject).append("<div class='project-description'><p>Description\
+				of the project.</p></div>"); 
+		}
+		else {
+			description.html("<p>Stop clicking on me! You just did it at" + (new Date()) + "</p>");
+			$(description).fadeOut();	
+		}
+	}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
